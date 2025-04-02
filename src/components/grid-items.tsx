8607ddf443c2293374e61d117dbface947766ede
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-04-02 07:44:35 -0500

    cambios en los grid

--- src/components/grid-items.tsx
+++ src/components/grid-items.tsx
@@ -6,7 +6,13 @@ interface GridItemProps {
     return (
         <section className={`hover:scale-102 transition-all duration-100 aspect-video bg-gray-300 ${iSelected ? "" : ""} overflow-hidden`}>
         <article className="w-full h-full flex flex-col">
-            <header className="sr-only">Grid Item</header> 
+            <figure className="w-full h-3/4 overflow-hidden">
+            <img className=" h-16 w-48" src="/volante.jpeg" alt="" />
+            </figure>
+            <section className="p-3 flex-grow">
+                <h2 className="font-medium text-gray-900 truncate">Event pinacoteca</h2>
+                <p className="tex-sm text-gray-600 line-clamp-2">The event is led by the University's Culutra, Bienestar and Muisca area with live presentations with the participation of students </p>
+            </section>
         </article>
         </section>
     )
