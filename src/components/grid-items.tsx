e1d726d66a354a59fdae81cf4ef0854ff1266168
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-04-02 07:30:51 -0500

    ultimo commit

--- src/components/grid-items.tsx
+++ src/components/grid-items.tsx
@@ -4,8 +4,8 @@ interface GridItemProps {
 
     export function GridItem({ iSelected = false }: GridItemProps) {
     return (
-        <section className={`hover:scale-102 transition-all duration-100 aspect-video bg-gray-300 ${iSelected ? "" : ""}`}>
-        <article className="w-full h-full">
+        <section className={`hover:scale-102 transition-all duration-100 aspect-video bg-gray-300 ${iSelected ? "" : ""} overflow-hidden`}>
+        <article className="w-full h-full flex flex-col">
             <header className="sr-only">Grid Item</header> 
         </article>
         </section>
