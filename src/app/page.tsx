e178c8e1851fee031a2fdac0f2f5d3b2a345b116
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-04-01 19:48:15 -0500

    cambios de utillidades

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,20 @@ import { GridItem } from "@/components/grid-items";
   export default function Home() {
     return (
       <main className="min-h-screen bg-black">
-        <header className=" bg-gray-200 p-4 flex justify-between items-center">
+        <span className=" bg-gray-200 p-4 flex justify-between items-center">
           <Logo />
           <MenuButton />
-        </header>
+        </span>
   
         <section className="p-4 bg-gray-800 mx-4 my-4 border">
-        <ul className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <section className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <GridItem  />
           <GridItem />
           <GridItem />
           <GridItem />
           <GridItem />
           <GridItem />
-        </ul>
+        </section>
           <h1 className=" text-white text-3xl font-extrabold">Bienvenidos a Eventos y Talentos Ucc </h1>
         </section>
       </main>
--- src/components/menu-button.tsx
+++ src/components/menu-button.tsx
@@ -17,28 +17,28 @@ export function MenuButton() {
 
         {isOpen && (
         <section className="absolute right-0 mt-2 w-48 bg-white rounded shadow-md">
-            <ul className="py-1">
-            <li>
+            <span className="py-1">
+            <section>
                 <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
                 Home
                 </a>
-            </li>
-            <li>
+            </section>
+            <section>
                 <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
                 Products
                 </a>
-            </li>
-            <li>
+            </section>
+            <section>
                 <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
                 Services
                 </a>
-            </li>
-            <li>
+            </section>
+            <section>
                 <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
                 Contact
                 </a>
-            </li>
-            </ul>
+            </section>
+            </span>
         </section>
         )}
     </nav>
